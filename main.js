08263e43953f348e4940848f2dc46170dc58c4ca
Yashika2021 <88836028+Yashika2021@users.noreply.github.com>
2022-12-29 22:12:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-function preload(){
+nose_x = 0;
+nose_y = 0;
 
+function preload(){
+    clown_nose = loadImage(" https://i.postimg.cc/3x3QzSGq/m.png")
 }
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
-}
-function draw(){
+    video= createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
 
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, nose_x, nose_y, 30, 30);
+}
+function modelLoaded(){
+    console.log("Model loaded");
 }
 function take_snapshot(){
     save("myFilterImage.png");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        nose_x =  results[0].pose.nose.x-15;
+        nose_y =  results[0].pose.nose.y-15;
+    }
 }
